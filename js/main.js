ddd495056645ed9981e9206cc190c200d1973347
Dilan kanishka <dilankanishka032@gmail.com>
2024-04-14 11:36:02 +0530

    With out css

---
+++ js/main.js
@@ -0,0 +1,23 @@
+import { myApiKey } from "./config.js";
+
+async function fetchData() {
+    const url = 'https://quotes15.p.rapidapi.com/quotes/random/';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': myApiKey,
+            'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        const quoteContainer = document.getElementById('quote-container');
+        quoteContainer.innerHTML = `<p>"${result.content}" - ${result.originator.name}</p>`;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+fetchData();
